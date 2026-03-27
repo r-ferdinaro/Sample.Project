9ed5a10d60ab72d72a7766dd57debc61d9593329
r-ferdinaro <r.ferdinaro@tricentis.com>
2026-03-27 17:36:50 +0300

    Add randomBallReduce functionality

--- js/main.js
+++ js/main.js
@@ -14,10 +14,24 @@ function onBallClick(idx, maxDiameter) {
 
 function swapBallsColor() {
     const elBall1 = document.querySelector(`.ball1`)
-    const ball1Color = window.getComputedStyle(elBall1).backgroundColor
     const elBall2 = document.querySelector(`.ball2`)
+    const ball1Color = window.getComputedStyle(elBall1).backgroundColor
     const ball2Color = window.getComputedStyle(elBall2).backgroundColor
 
     elBall1.style.backgroundColor = ball2Color
     elBall2.style.backgroundColor = ball1Color
+}
+
+function randomBallReduce() {
+    const elBall1 = document.querySelector(`.ball1`)
+    const elBall2 = document.querySelector(`.ball2`)
+    const orgBall1Size = parseFloat(window.getComputedStyle(elBall1).width)
+    const orgBall2Size = parseFloat(window.getComputedStyle(elBall2).width)
+    
+    const reduceVal = getRandomInt(20, 61)
+    let ball1Size = orgBall1Size - reduceVal
+    let ball2Size = orgBall2Size - reduceVal
+
+    elBall1.style.width = (ball1Size > 100) ? `${ball1Size}px` : `100px`
+    elBall2.style.width = (ball1Size > 100) ? `${ball1Size}px` : `100px`
 }
