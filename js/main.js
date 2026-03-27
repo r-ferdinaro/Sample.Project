765cb9c3974f7803b7e6168b2a31fb662f287447
r-ferdinaro <r.ferdinaro@tricentis.com>
2026-03-27 17:07:14 +0300

    Refactor onBallClick

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@
 
 function onBallClick(idx) {
     const elBall = document.querySelector(`.ball${idx}`)    
-    const ballSize = getRandomInt(20, 61)
+    const orgBallSize = parseFloat(window.getComputedStyle(elBall).width)
+    const increment = getRandomInt(20, 61)
+    
+    const ballSize = orgBallSize + increment
     const ballColor = getRandomColor()
 
-    elBall.style.width = `${ballSize}px`
+    elBall.style.width = (ballSize < 400) ? `${ballSize}px` : `100px`
     elBall.style.backgroundColor = ballColor
 }
