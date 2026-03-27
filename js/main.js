f5a30e793061d3a0e26eaa60bbd958a1c0ecbc6a
r-ferdinaro <r.ferdinaro@tricentis.com>
2026-03-27 17:24:20 +0300

    Add swapBallsColor

--- js/main.js
+++ js/main.js
@@ -11,3 +11,13 @@ function onBallClick(idx, maxDiameter) {
     elBall.style.width = (ballSize < maxDiameter) ? `${ballSize}px` : `100px`
     elBall.style.backgroundColor = ballColor
 }
+
+function swapBallsColor() {
+    const elBall1 = document.querySelector(`.ball1`)
+    const ball1Color = window.getComputedStyle(elBall1).backgroundColor
+    const elBall2 = document.querySelector(`.ball2`)
+    const ball2Color = window.getComputedStyle(elBall2).backgroundColor
+
+    elBall1.style.backgroundColor = ball2Color
+    elBall2.style.backgroundColor = ball1Color
+}
