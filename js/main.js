2c90d0d9f1662df7c8ffdadbdff06b04af0db6cd
r-ferdinaro <r.ferdinaro@tricentis.com>
2026-03-27 16:40:45 +0300

    Ball Reset size when more than 400

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ function onBallClick() {
     const elBall = document.querySelector('.ball')
     const orgBallSize = parseFloat(window.getComputedStyle(elBall).width)
     const increment = 50
+    let ballSize = orgBallSize + increment
 
-    elBall.style.width = `${orgBallSize + increment}px`
+    if (ballSize > 400) ballSize = 100
+
+    elBall.style.width = `${ballSize}px`
 }
